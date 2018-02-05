acb642a37905b1a7eaf02f3308e50fbebc321017
Massimiliano Arione <garakkio@gmail.com>
2018-02-05 09:16:20 +0100

    make it work with phpunit7 (#54)
    * make it work with phpunit7
    
    * drop php 7.0
    
    * improve if and semver
    
    * fix type hint

--- src/DAMA/DoctrineTestBundle/PHPUnit/PHPUnitListener.php
+++ src/DAMA/DoctrineTestBundle/PHPUnit/PHPUnitListener.php
@@ -4,44 +4,41 @@ namespace DAMA\DoctrineTestBundle\PHPUnit;
 
 use DAMA\DoctrineTestBundle\Doctrine\DBAL\StaticDriver;
 
-if (!class_exists('\PHPUnit_Framework_BaseTestListener')) {
-    // PHPUnit 6+
+if (class_exists('\PHPUnit\Framework\BaseTestListener')) {
+    // PHPUnit 6
     class PHPUnitListener extends \PHPUnit\Framework\BaseTestListener
     {
         public function startTest(\PHPUnit\Framework\Test $test)
         {
             StaticDriver::beginTransaction();
         }
-
         public function endTest(\PHPUnit\Framework\Test $test, $time)
         {
             StaticDriver::rollBack();
         }
-
         public function startTestSuite(\PHPUnit\Framework\TestSuite $suite)
         {
             StaticDriver::setKeepStaticConnections(true);
         }
     }
-} else {
-    // PHPUnit 5
-    class PHPUnitListener extends \PHPUnit_Framework_BaseTestListener
+} elseif (trait_exists('\PHPUnit\Framework\TestListenerDefaultImplementation')) {
+    // PHPUnit 7+
+    class PHPUnitListener implements \PHPUnit\Framework\TestListener
     {
-        public function startTest(\PHPUnit_Framework_Test $test)
+        use \PHPUnit\Framework\TestListenerDefaultImplementation;
+
+        public function startTest(\PHPUnit\Framework\Test $test): void
         {
             StaticDriver::beginTransaction();
         }
-
-        public function endTest(\PHPUnit_Framework_Test $test, $time)
+        public function endTest(\PHPUnit\Framework\Test $test, float $time): void
         {
             StaticDriver::rollBack();
         }
-
-        public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
+        public function startTestSuite(\PHPUnit\Framework\TestSuite $suite): void
         {
             StaticDriver::setKeepStaticConnections(true);
         }
     }
 }
-
 
