f166fed1c902807db361c125798741e4a78a06ce
Francesco Montanari <francesco.montanari@outlook.com>
2018-12-09 12:45:39 +0200

    Fix deprecation for symfony/config 4.2 (#69)
    * Fix deprecation for symfony/config 4.2
    
    Fixed the same way as in sensiolabs/SensioFrameworkExtraBundle@be6a8cc1c1ecfe9489fb51e4869af15a13fc2cd2
    
    * fix variable name

--- src/DAMA/DoctrineTestBundle/DependencyInjection/Configuration.php
+++ src/DAMA/DoctrineTestBundle/DependencyInjection/Configuration.php
@@ -16,8 +16,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $root = $treeBuilder->root('dama_doctrine_test');
+        $treeBuilder = new TreeBuilder('dama_doctrine_test');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $root = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $root = $treeBuilder->root('dama_doctrine_test');
+        }
 
         $root
             ->addDefaultsIfNotSet()
