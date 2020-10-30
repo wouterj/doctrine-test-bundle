c320713feea8b902dca9003d021502e753be156e
Wouter J <wouterj@users.noreply.github.com>
2020-10-30 12:29:32 +0100

    Improve Travis build output (#133)
    * Register a NullLogger to avoid logging output
    * Use Travis output folding
    * Show PHPCSFixer changes upon failure

--- makefile
+++ makefile
@@ -20,7 +20,7 @@ php_cs_fixer_fix: php-cs-fixer.phar
 	./php-cs-fixer.phar fix --config .php_cs src tests
 
 php_cs_fixer_check: php-cs-fixer.phar
-	./php-cs-fixer.phar fix --config .php_cs src tests --dry-run
+	./php-cs-fixer.phar fix --config .php_cs src tests --dry-run --diff --diff-format=udiff
 
 php-cs-fixer.phar:
 	wget https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.16.6/php-cs-fixer.phar && chmod 777 php-cs-fixer.phar
--- tests/Functional/app/AppKernel.php
+++ tests/Functional/app/AppKernel.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Functional\app;
 
+use Psr\Log\NullLogger;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
@@ -19,5 +21,10 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__.'/config.yml');
+    }
+
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->register('logger', NullLogger::class);
     }
 }
